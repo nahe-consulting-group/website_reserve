e2c43cdde5a3987438f796c397971b2d86fc1e64
filoquin <filoquin@gmail.com>
2020-05-15 10:42:00 -0300

    nuevo archivo:  models/payment.py

--- __manifest__.py
+++ __manifest__.py
@@ -7,13 +7,13 @@
     'description': """
         Create picking for reserve products in website
     """,
-
+    'sequence':30,
     'author': "filoquin",
     'website': "http://www.sipecu.com.ar",
 
     'category': 'website',
     'version': '11.0.1.0.1',  
-    'depends': ['website_sale'],
+    'depends': ['website_sale','sale_payment'],
 
     # always loaded
     'data': [
